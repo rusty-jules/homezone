286314ab97ae1030a887cb21140a7e2a4e1582d0
Julian Aichholz <julianaichholz@gmail.com>
2023-04-09 20:19:20 -0700

    feat: add deploy-rs config

--- flake.nix
+++ flake.nix
@@ -8,9 +8,11 @@
 
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    deploy-rs.url = github:serokell/deploy-rs;
   };
 
-  outputs = { self, nixpkgs, sops-nix, ... } @ inputs:
+  outputs = { self, nixpkgs, sops-nix, deploy-rs, ... } @ inputs:
     let
       system = "x86_64-linux";
       inherit (inputs.nixpkgs.lib) mapAttrs;
@@ -19,11 +21,42 @@
       nixosConfigurations =
         import ./outputs/nixos-conf.nix { inherit inputs system sops-nix; };
 
-      checks.${system} =
+      deploy.nodes = {
+        jables = {
+          sshUser = "root";
+          hostname = "jables";
+          remoteBuild = true;
+          profiles.system = {
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.jables;
+            remoteBuild = true;
+          };
+        };
+        kables = {
+          sshUser = "root";
+          hostname = "kables";
+          remoteBuild = true;
+          profiles.system = {
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.kables;
+            remoteBuild = true;
+          };
+        };
+        platy = {
+          sshUser = "root";
+          hostname = "platy";
+          remoteBuild = true;
+          profiles.system = {
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.platy;
+            remoteBuild = true;
+          };
+        };
+      };
+
+      checks =
         let
           os = mapAttrs (_: c: c.config.system.build.toplevel) nixosConfigurations;
+          deploy = mapAttrs(system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
         in
-        os;
+        deploy;
 
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
     };
