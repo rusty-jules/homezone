a90926d71fb1ef813aec22e0b191dddc81120467
Julian Aichholz <julianaichholz@gmail.com>
2023-05-01 01:21:40 -0700

    feat: first add cuda as a substitutor

--- apps/cuda.nix
+++ apps/cuda.nix
@@ -2,6 +2,11 @@
 {
   environment.systemPackages = with pkgs; [
     cudatoolkit
+    # enable nvidia-docker, which is oddly defined in multiple places
+    # in nixpkgs. Guide here was great:
+    # https://sebastian-staffa.eu/posts/nvidia-docker-with-nix/
+    # https://github.com/NixOS/nixpkgs/blob/db3e8325a9b62b2b4fad0342f6835cb4ccc80c9b/pkgs/applications/virtualization/nvidia-docker/default.nix
+    nvidia-docker
   ];
 
   # Nvidia Driver, GTX 1070 is not yet legacy!
