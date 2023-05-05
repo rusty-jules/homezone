97b85de24d1adb7634b92c8d4bdb6feacf5a7f51
Julian Aichholz <julianaichholz@gmail.com>
2023-05-05 06:35:15 -0700

    feat: raise file ulimit

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,13 @@
     defaultSopsFile = ./secrets.enc.yml;
   };
 
+  # increase maximum number of open files
+  # https://github.com/NixOS/nixpkgs/issues/159964
+  systemd.services."user@1000".serviceConfig.LimitNOFILE = "32768";
+  security.pam.loginLimits = [
+    {domain = "*"; type = "-"; item = "nofile"; value = "32768";}
+  ];
+
   system.copySystemConfiguration = lib.mkDefault false;
 
   system.stateVersion = lib.mkDefault "22.11";
