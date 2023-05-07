7a0d727afb72a366a05910d165033ee6fdf2e5c1
Julian Aichholz <julianaichholz@gmail.com>
2023-05-07 03:00:42 -0700

    feat: auto-authorize thunderbolt on jables

--- nodes/jables.nix
+++ nodes/jables.nix
@@ -27,6 +27,10 @@
   # thunderbolt 3 management
   services.hardware.bolt.enable = true;
 
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
+  '';
+
   system.copySystemConfiguration = false;
   system.stateVersion = "22.11";
 }
