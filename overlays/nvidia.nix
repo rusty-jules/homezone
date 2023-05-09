24ae276ca32a0c13ff2d9d1f1f290e93ab79915d
Julian Aichholz <julianaichholz@gmail.com>
2023-05-08 19:02:25 -0700

    feat: nvidia-container-toolkit

--- overlays/nvidia.nix
+++ overlays/nvidia.nix
@@ -1,8 +1,22 @@
 self: super:
+let
+  nvidia-modprobe-version = "495.44";
+  nvidia-modprobe-src = super.fetchurl {
+    url = "https://github.com/NVIDIA/nvidia-modprobe/archive/${nvidia-modprobe-version}.tar.gz";
+    sha256 = "sha256-rm6cfmtDNolFwo9ri20NfMNu5+G+iVWgCaHLGJ5G3pI=";
+  };
+
+  elftoolchain-version = "elftoolchain-0.7.1";
+  elftoolchain-src = super.fetchurl {
+    url = "https://sourceforge.net/projects/elftoolchain/files/Sources/${elftoolchain-version}/${elftoolchain-version}.tar.bz2";
+    sha256 = "1dfj5fxvlsqa88rcyxpl88pyqjzvydi7bp8mf8w984pjzj8lbwa4";
+  };
+in
 {
   nvidia-container-toolkit = super.stdenv.mkDerivation {
     pname = "nvidia-container-toolkit";
     version = "1.13.1";
+
     src = super.fetchgit {
       url = "https://github.com/NVIDIA/libnvidia-container";
       rev = "refs/tags/v1.13.1";
@@ -10,14 +24,59 @@ self: super:
       leaveDotGit = true;
     };
 
-    nativeBuildInputs = with super.pkgs; [
-      git
+    preConfigure = ''
+      # add libtirpc to gcc
+      export CFLAGS="-I${super.lib.getDev super.pkgs.libtirpc}/include/tirpc $CFLAGS"
+      export LDFLAGS="-L${super.lib.getLib super.pkgs.libtirpc}/lib -ltirpc $LDFLAGS"
+    '';
+
+    postUnpack = ''
+      mkdir -p $sourceRoot/deps/src/nvidia-modprobe-${nvidia-modprobe-version}
+      cat ${nvidia-modprobe-src} | tar -C $sourceRoot/deps/src/nvidia-modprobe-${nvidia-modprobe-version} \
+        --strip-components=1 -xz nvidia-modprobe-${nvidia-modprobe-version}/modprobe-utils
+      mkdir -p $sourceRoot/deps/src/${elftoolchain-version}
+      cat ${elftoolchain-src} | tar -C $sourceRoot/deps/src/${elftoolchain-version}/ \
+        --strip-components=1 -xj ${toString (map(dir: "${elftoolchain-version}/${dir}") ["mk" "common" "libelf"])}
+    '';
+
+    buildInputs = with super.pkgs; [
+      libcap
+      libseccomp
+      # there is a mk/libtirpc.mk that downloads and builds the source,
+      # but it was not run by default `make` for some reason, likely the WITH_TIRPC flag
+      libtirpc
     ];
 
-    patchPhase = ''
+    nativeBuildInputs = with super.pkgs; [
+      bmake
+      gnum4
+      git
+      go
+      lsb-release
+      pkg-config
+      rpcsvc-proto
+      stdenv
+      which
+    ];
+
+    prePatch = ''
       substituteInPlace Makefile --replace \
       "/usr/local" \
       $out/usr/local
+    '';
+
+    patches = [ ./remove-curls.patch ];
+
+    buildPhase = ''
+      export GOCACHE=$NIX_BUILD_TOP/.cache
+      export GOPATH=$NIX_BUILD_TOP/go
+      make
+    '';
+
+    installPhase = ''
+      # ensure we do not use bmake, which was required for elftoolchain,
+      # but is not compatible with the root Makefile
+      make install prefix=$out exec_prefix=$out
     '';
   };
 }
