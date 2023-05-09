5b895ac4a9cfb8bc3c5fe74e18087fe69d9babcf
Julian Aichholz <julianaichholz@gmail.com>
2023-05-09 01:58:29 -0700

    feat: install nvidia-container-toolkit on all nodes

--- apps/default.nix
+++ apps/default.nix
@@ -21,6 +21,9 @@
     # apps required for additional k3s services
     # for longhorn
     nfs-utils
+    # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#id6
+    # https://itnext.io/enabling-nvidia-gpus-on-k3s-for-cuda-workloads-a11b96f967b0
+    nvidia-container-toolkit
   ] ++ lib.optionals (pkgs.system != "armv7l-linux") [
     fd
     sd
