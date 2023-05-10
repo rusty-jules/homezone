a0cc6dc682ed37793330afe4e2298db33a140df3
Julian Aichholz <julianaichholz@gmail.com>
2023-05-10 01:28:43 -0700

    feat: add ha-server config for etcd

---
+++ k3s/ha-server.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [ ./base.nix ];
+  sops.secrets.k3s_node_token2 = { };
+  services.k3s = {
+    role = "server";
+    tokenFile = lib.mkDefault config.sops.secrets.k3s_node_token2.path;
+    serverAddr = lib.mkDefault "https://192.168.1.71:6443";
+    extraFlags = toString [
+      "--node-ip ${config.networking.homezone.currentHost.ipv4}"
+    ];
+  };
+}
