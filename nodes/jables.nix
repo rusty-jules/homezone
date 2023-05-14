469d8ed0d1f1456abc36b8845cd66c6416ccfd98
Julian Aichholz <julianaichholz@gmail.com>
2023-05-13 17:55:50 -0700

    fix: add additional ether config for jables
    jables is tricky given the thunderbolt eth and its status as a master
    node that cannot go down

--- nodes/jables.nix
+++ nodes/jables.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -31,9 +31,40 @@
     ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
   '';
 
-  networking.defaultGateway = {
-    address = "192.168.1.1";
-    interface = config.networking.homezone.currentHost.etherInterfaceName;
+  networking = {
+    defaultGateway = {
+      address = "192.168.1.1";
+      interface = config.networking.homezone.currentHost.etherInterfaceName;
+    };
+
+    interfaces = {
+      "enp7s0u1u4" = {
+        ipv4.addresses = lib.mkIf (config.networking.homezone.currentHost.etherIp != null) [{
+          address = config.networking.homezone.currentHost.etherIp;
+          prefixLength = 24;
+        }];
+      };
+
+      ${config.networking.homezone.currentHost.etherInterfaceName}.ipv4.routes = [
+        {
+          options.scope = "global";
+          address = "192.168.1.0";
+          prefixLength = 24;
+          via = config.networking.homezone.currentHost.etherIp;
+        }
+      ];
+
+      ${config.networking.homezone.currentHost.wifiInterfaceName}.ipv4.routes = map
+        (via: {
+          options.scope = "global";
+          # lower the priority of the wifi interface for the 192.168.1.0/24 subnet
+          options.metric = "100";
+          address = "192.168.1.0";
+          prefixLength = 24;
+          via = via;
+        }) with config.networking.homezone.currentHost;
+      [ etherIp ipv4 ] ++ [ "0.0.0.0" ];
+    };
   };
 
   system.copySystemConfiguration = false;
