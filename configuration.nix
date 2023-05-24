162dfc440c50cd1da90267cd52fe3602d6a070d9
Julian Aichholz <julianaichholz@gmail.com>
2023-05-23 23:48:23 -0700

    fix: raise a bunch of file limits

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,12 @@
   # increase maximum number of open files
   # https://github.com/NixOS/nixpkgs/issues/159964
   # etcd recommends 64,000
+  boot.kernel.sysctl = {
+    "fs.file-max" = 1000000;
+    # https://community.harness.io/t/failed-to-create-fsnotify-watcher-too-many-open-files/11945
+    "fs.inotify.max_user_watches" = 100000;
+    "fs.inotify.max_user_instances" = 100000;
+  };
   systemd.services."user@1000".serviceConfig.LimitNOFILE = "524288";
   security.pam.loginLimits = [
     { domain = "*"; type = "-"; item = "nofile"; value = "524288"; }
